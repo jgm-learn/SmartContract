db2d56b01c0067c2eb6aede0b859068f740facac
jgm-learn <31833970+jgm-learn@users.noreply.github.com>
2017-09-11 14:08:33 +0800

    仓单业务智能合约（无权限控制、无资金管理）

---
+++ ID.sol
@@ -0,0 +1,23 @@
+﻿pragma solidity ^0.4.5;
+
+
+
+contract ID_contract
+{
+    string      user_id_;       //客户id
+    uint        receipt_id_;    //仓单id
+    uint        con_id_;        //合同id
+    uint        neg_id_;        //协商id
+    
+    //创建合同id
+    function contract_id() returns(uint )
+    {
+        return ++con_id_;
+    }
+    
+    //创建协商交易编号
+    function negotiate_id() returns(uint )
+    {
+        return  ++neg_id_;
+    }
+}
